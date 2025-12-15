cfe0345cefcf0e746a3ff826e6e3a725941e4d3b
janex0 <jan.meh007@gmail.com>
2025-12-15 14:11:11 +0100

    google

--- projekt_recepti2/app/api/auth/[...nextauth]/route.ts
+++ projekt_recepti2/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
   providers: [
@@ -8,7 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
   secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async signIn({ user, account }) {
+      // samo Google
+      if (account?.provider === "google") {
+        if (!user.email) return false;
+
+        // preveri če user že obstaja
+        let dbUser = await prisma.user.findUnique({
+          where: { email: user.email },
+        });
+
+        // če ne obstaja → ustvarimo
+        if (!dbUser) {
+          dbUser = await prisma.user.create({
+            data: {
+              email: user.email,
+              name: user.name,
+              googleId: account.providerAccountId,
+            },
+          });
+        }
+
+        return true;
+      }
+
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
