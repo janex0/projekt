0fa7af35d14e17f2070966a4ec23434adab7afc4
janex0 <jan.meh007@gmail.com>
2025-12-15 23:38:40 +0100

    yx,c

--- projekt_recepti2/app/layout.tsx
+++ projekt_recepti2/app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css';
 import Header from '../components/Header';
+import Providers from './providers';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sl">
       <body className="bg-white text-black">
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
